c2bd8bec60f3aeeb589673b4f2d6d3f214d49da7
Abdelkaim El Halabi <79865489+abdelkarimhalabi@users.noreply.github.com>
2022-01-01 19:36:52 +0200

    Update images.service.ts

--- src/app/shared/images.service.ts
+++ src/app/shared/images.service.ts
@@ -5,7 +5,7 @@ import { catchError,tap,map } from "rxjs/operators";
 @Injectable()
 export class ImageService{
     private query: any;
-    private API_KEY: string = '23246224-2025e6ab3cf7d78fb6f77c7d1';
+    private API_KEY: string = '';
     private API_URL: string = "https://pixabay.com/api/?key=";
     private URL: string = this.API_URL + this.API_KEY + '&q=';
     private perPage: string = "&per_page=12";
